93f25ce3f9dc1b423699212220d51eb71a42f1ff
勒于驴 <1404305637@qq.com>
2022-11-20 19:30:33 +0800

    feat: 完成upload组件的开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { IconPack, library } from '@fortawesome/fontawesome-svg-core'
-
-import Button, { ButtonSize, ButtonType } from './components/Button/button';
-import MenuItem from './components/Menu/menuItem';
-import Menu from './components/Menu/menu';
-import SubMenu from './components/Menu/subMenu';
-import Icon from './components/Icon/icon';
-import { Input } from './components/Input/input';
-library.add(fas as IconPack)
+import React, { useEffect, useState } from 'react';
+//@ts-ignore
+import axios from 'axois'
 const App: React.FC = () => {
+  const [title, setTitle] = useState('')
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts', {
+      headers: {
+        'X-Requested-With': 'XMLHTttpRequest',
+      },
+      responseType: 'json'
+    }).then(res => {
+      console.log(res)
+      setTitle(res)
+    })
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <Input></Input>
       </header>
     </div>
   );
